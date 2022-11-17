397273c26d24015aa103412d57ce95ddf64c3f5c
Emmanuel <79337127+CarlosErz@users.noreply.github.com>
2022-11-17 17:09:29 -0600

    Add files via upload

---
+++ js/animaciones.js
@@ -0,0 +1,10 @@
+window.sr = ScrollReveal({ reset: true })
+sr.reveal('.fotos',
+ { duration: 2000,
+  origin: 'top',
+
+});
+sr.reveal('.heart', {
+  rotate: { x: 40, y: 100, z: 0 },
+duration: 1500,
+});
